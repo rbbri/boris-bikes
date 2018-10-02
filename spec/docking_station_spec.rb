9a05080f96627f0c2b718f5ce4973b216a256625
Robin Johnson <robin@Robins-MacBook-Pro.local>
2018-10-02 10:43:50 +0100

    Added functionality to pass working test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,7 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 end
+
+describe DockingStation.new.release_bike do
+  it { is_expected.to be_working }
+end
