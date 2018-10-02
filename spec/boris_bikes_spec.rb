85590e300c2a83c6a3562ed4df46d27c2f9372fb
Robin Johnson <robin@Robins-MacBook-Pro.local>
2018-10-02 10:16:48 +0100

    Added release_bike method

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,8 @@
 require "docking_station"
 
 describe DockingStation do
+  describe "should respond to release bike method" do
+  it { expect(DockingStation.new).to respond_to(:release_bike) }
+  end
 
 end
