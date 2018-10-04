5cc62d1df4e11453a1bb23f545c7611c0fd2ec00
rbbri <robin.s.johnson@gmail.com>
2018-10-04 16:00:02 +0100

    Isolated tests by mocking behaviour

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,8 @@ require "docking_station"
 #
 describe DockingStation do
 
+  let(:bike) { double(:bike) }
+
   it { is_expected.to respond_to :release_bike }
 
   it { is_expected.to respond_to :dock }
@@ -12,8 +14,8 @@ describe DockingStation do
     end
 
     it 'releases a bike that should be working' do
+      allow(bike).to receive(:working?).and_return(true)
       station = subject
-      bike = Bike.new
       station.dock(bike)
       expect(station.release_bike).to be_working
     end
@@ -22,30 +24,32 @@ describe DockingStation do
 
   describe '#dock' do
     it 'displays a message when a bike is docked' do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq "#{bike} successfully docked"
     end
 
     it 'raises an error when the dock is already full' do
       station = subject
-      station.capacity.times { station.dock(Bike.new) }
-      expect { station.dock(Bike.new) }.to raise_error("Dock full!")
+      station.capacity.times { station.dock(bike) }
+      expect { station.dock(bike) }.to raise_error("Dock full!")
     end
 
     it 'allows a user to report a broken bike when they return it' do
+      allow(bike).to receive(:working?).and_return(false)
+      allow(bike).to receive(:report)
       station = DockingStation.new
-      bike = Bike.new
       station.dock(bike, true)
       expect(station.bikes[0].working?).to eq false
     end
 
     it "doesn't release broken bikes" do
+      allow(bike).to receive(:working?).and_return(true)
+      allow(bike).to receive(:report)
       station = DockingStation.new
-      bike_broken = Bike.new
+      bike_broken = bike
       station.dock(bike_broken, true)
-      bike_working = Bike.new
+      bike_working = bike
       station.dock(bike_working, false)
-      second_bike_broken = Bike.new
+      second_bike_broken = bike
       station.dock(second_bike_broken, true)
       released_bike = station.release_bike
       expect(released_bike).to eq bike_working
